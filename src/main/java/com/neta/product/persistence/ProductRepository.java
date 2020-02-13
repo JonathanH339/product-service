48af646ec4dc37d3d69a55f8f5bc0efe9745de3b
Jonathan <jonathanhughes123@gmail.com>
2020-02-13 14:37:59 -0500

    Changed MongoRepository to PagingAndSortingRepository

--- src/main/java/com/neta/product/persistence/ProductRepository.java
+++ src/main/java/com/neta/product/persistence/ProductRepository.java
@@ -2,13 +2,13 @@ package com.neta.product.persistence;
 
 import java.util.Optional;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.neta.product.entity.ProductEntity;
 
 @Repository
-public interface ProductRepository extends MongoRepository<ProductEntity, String> {
+public interface ProductRepository extends PagingAndSortingRepository<ProductEntity, String> {
 	
 	Optional<ProductEntity> findByProductId(int productId);
 
