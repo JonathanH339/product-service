0fa77169d214d1cf87c04796a7e703f293f4b700
Jonathan <jonathanhughes123@gmail.com>
2020-02-20 19:27:50 -0500

    Fix mapper no longer returns null causing null pointer

--- src/main/java/com/neta/product/entity/ProductMapper.java
+++ src/main/java/com/neta/product/entity/ProductMapper.java
@@ -1,5 +1,3 @@
-
-
 package com.neta.product.entity;
 
 import org.springframework.stereotype.Component;
@@ -8,37 +6,28 @@ import com.neta.api.core.product.Product;
 
 @Component
 public class ProductMapper {
-	
-	public Product entityToApi(ProductEntity entity)
-	{
-		if(entity == null) {
-			return null;
-		}
-		
+
+	public Product entityToApi(ProductEntity entity) {
+
 		Product newProduct = new Product();
-		
+
 		newProduct.setProductId( entity.getProductId() );
 		newProduct.setName( entity.getName() );
 		newProduct.setDescription( entity.getDescription() );
 		newProduct.setPrice( entity.getPrice() );
-		
+
 		return newProduct;
 	}
-	
+
 	public ProductEntity apiToEntity(Product product) {
-		
-		if(product==null) {
-			return null;
-		}
-		
+
 		ProductEntity newEntity = new ProductEntity();
-		
+
 		newEntity.setProductId( product.getProductId() );
 		newEntity.setName( product.getName() );
 		newEntity.setDescription( product.getDescription() );
 		newEntity.setPrice( product.getPrice() );
-		
+
 		return newEntity;
-		
 	}
 }
